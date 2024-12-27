f57f19bbf15e66f63a9cadc9bcdbf10b8e8a7bd1
syedjamal12 <mdhasnat067@gmail.com>
2024-12-28 00:25:03 +0530

    api done

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,25 @@
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
 
 export default function Home() {
   return (
-    <div >
-     Hii
-    </div>
+    <div className="min-h-screen flex flex-col ">
+    {/* Header */}
+    <Navbar  />
+    {/* Hero Section */}
+    <HeroSection />
+
+    {/* Features Section */}
+    <FeatureSection />
+
+    {/* User Reviews Section */}
+    <UserReviews />
+
+    {/* Footer */}
+    <Footer />
+  </div>
   );
 }

3cea15e15f764fccec0dfe79dd498ccb98d21819
syedjamal12 <mdhasnat067@gmail.com>
2024-12-29 00:14:44 +0530

    add Authentication part

--- front/src/app/api/auth/[...nextauth]/options.ts
+++ front/src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,44 @@
-import NextAuth from "next-auth"
+import NextAuth, { ISODateString } from "next-auth"
 import { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import dotenv from "dotenv";
+import { JWT } from "next-auth/jwt";
+dotenv.config();
+
+export interface CustomSession {
+  user?:CustomUser;
+  expires:ISODateString
+}
+export interface CustomUser{
+  id?:string|null,
+  name?:string|null,
+  email?:string|null,
+  image?:string|null,
+  provider?:string|null,
+  token?:string|null
+
+}
+
 export const authOptions: AuthOptions = {
     pages:{
         signIn: "/",
     },
+
+  
+  callbacks: {
+    
+    async session({ session, user, token }:{session:CustomSession,user:CustomUser,token:JWT}) {
+      session.user=token.user as CustomUser
+      return session
+    },
+    async jwt({ token, user}) {
+      if(user){
+        token.user=user
+      }
+      return token
+    }
+
+},
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({

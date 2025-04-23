618255c425c8d639b59148881efb3419e5489e41
syedjamal12 <mdhasnat067@gmail.com>
2025-04-23 10:42:40 +0530

    gemini ai

--- server/src/config/ai.config.ts
+++ server/src/config/ai.config.ts
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-const prompt = "Explain how AI works";
+const prompt = "Explain how AI working";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
